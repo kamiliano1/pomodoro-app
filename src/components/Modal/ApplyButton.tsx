bc461d7efc35365176d2f4206b8c939e4c00681a
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-05-03 22:08:05 +0200

    Update ApplyButton.tsx

--- src/components/Modal/ApplyButton.tsx
+++ src/components/Modal/ApplyButton.tsx
@@ -9,12 +9,10 @@ const ApplyButton: React.FC<ApplyButtonProps> = ({ applySettings }) => {
     <div className="w-full flex justify-center">
       <button
         onClick={applySettings}
-        className={`absolute mx-auto font-bold bg-${
+        className={`absolute px-12 py-[.9375rem] bottom-[-27px] hover:brightness-75 mx-auto font-bold bg-${
           activeSettings.color
         } rounded-[26.5px]
-       ${
-         activeSettings.color === "70F3F8" ? "text-161932" : "text-white"
-       }  px-12 py-[.9375rem] bottom-[-27px] `}
+       ${activeSettings.color === "70F3F8" ? "text-161932" : "text-white"}  `}
       >
         Apply
       </button>
